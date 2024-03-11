19149c29552fd9d533d0ca2c6e50b03ee2ca878e
FEDE\Fede <federicoandrescaivano@gmail.com>
2024-03-11 10:49:35 -0300

    Se agrego una linea que imprime la variable nota

--- mapbiomas-ejercicio-1.js
+++ mapbiomas-ejercicio-1.js
@@ -4,3 +4,12 @@ var image = ee.Image(0);
 print(image);
 
 Map.addLayer(image);
+
+var nota = 'Vamos a imprimir esta nota'
+print(nota)
+//notas
+//git init
+//git status
+//git add mapbiomas-ejercicio-1.js
+//git commit m-""
+//git log
